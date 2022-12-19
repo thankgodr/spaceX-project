617cd964df369f0dfbb1ae66188b25ffd16920fd
ThankGod Richard <thankgod@lemonade.finance>
2022-12-19 22:40:04 +0100

    Logo added to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,13 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { NavLink } from 'react-router-dom';
 import NavRoutePaths from '../navigation/navroutes';
+import Logo from "../images/logo.png"
 
 const Header = () => {
     return (
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
             <Container>
-                <Navbar.Brand href="#home">Space Travels' Hub</Navbar.Brand>
+                <Navbar.Brand href="/"> <img src={Logo} className="logo"/> Space Travels' Hub</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
