0f191150817c3598b1ceffff7e96098b20e5b245
hopeforkoami <hopeforkoami@gmail.com>
2022-12-20 23:26:08 +0000

    Add Mission Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -5,10 +5,15 @@ import { useSelector } from 'react-redux';
 
 const Profile = () => {
   const state = useSelector((state) => state.rockets);
+  const missionState = useSelector((state) => state.missions);
   return (
         <Container>
             <Row>
                 <Col md={6}>
+                <h3>My Missions</h3>
+                    <ListGroup>
+                        {missionState.filter((missions) => missions.reserved).map((mission) => <ListGroup.Item key={mission.id} className="p-3 text-left">{mission.name}</ListGroup.Item>)}
+                    </ListGroup>
                 </Col>
                 <Col md={6}>
                     <h3>My Rockets</h3>
