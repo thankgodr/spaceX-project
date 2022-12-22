a37dce25d8729c2633ff22830f7512fb9d4abd78
ThankGod Richard <thankgod@lemonade.finance>
2022-12-23 00:08:47 +0100

    fixes

--- src/__test__/missions.test.js
+++ src/__test__/missions.test.js
@@ -2,15 +2,16 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
 import Missions from '../components/Missions';
-jest.mock('axios'); 
+
+jest.mock('axios');
 
 describe('missions test', () => {
-  test('should render a mission component', () => { 
+  test('should render a mission component', () => {
     render(
       <Provider store={store}>
         <Missions/>
-    </Provider>
-      );
-      expect(screen.getByText('Mission')).toBeInTheDocument();
-  })
+    </Provider>,
+    );
+    expect(screen.getByText('Mission')).toBeInTheDocument();
+  });
 });
